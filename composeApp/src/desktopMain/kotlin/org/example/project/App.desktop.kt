2699788a3334de0574aedad1ec1627db80c27961
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-11-18 17:28:54 +1100

    Made changes to imap fetching increasing size and disabled partial fetches.

--- composeApp/src/desktopMain/kotlin/org/example/project/App.desktop.kt
+++ composeApp/src/desktopMain/kotlin/org/example/project/App.desktop.kt
@@ -16,18 +16,19 @@ import java.util.*
 
 
 actual class EmailService {
-    actual fun getEmails(emailDataSource: EmailDataSource, emailTableQueries: EmailTableQueries, accountQueries: AccountTableQueries, emailAddress: String, password: String): List<Email> {
+    actual suspend fun getEmails(emailDataSource: EmailDataSource, emailTableQueries: EmailTableQueries, accountQueries: AccountTableQueries, emailAddress: String, password: String): List<Email> {
 
         val properties: Properties = Properties().apply {
-            put("mail.store.protocol", "imap")
-            // put("mail.imap.ssl.trust", "imap.gmail.com")
+            put("mail.imap.host", "imap.gmail.com")
             put("mail.imap.username", emailAddress)
             put("mail.imap.password", password)
-            put("mail.imap.host", "imap.gmail.com")
             put("mail.imap.port", "993")
             put("mail.imap.ssl.enable", "true")
             put("mail.imap.connectiontimeout", 10000)
             put("mail.imap.timeout", 10000)
+            put("mail.imap.partialfetch", "false");
+            put("mail.imap.fetchsize", "1048576");
+
         }
 
         val session = Session.getInstance(properties)
@@ -36,12 +37,15 @@ actual class EmailService {
         // Check if emails exist in db
         val emailsExist = doEmailsExist(emailTableQueries, emailDataSource)
 
-        if (emailsExist) {
-            val emails = returnEmails(emailTableQueries, emailDataSource)
-            return emails
-        }
+//        if (emailsExist) {
+//            val emails = returnEmails(emailTableQueries, emailDataSource)
+//            return emails
+//        }
 
         val email: List<Email> = fetchEmailBodies(emailAddress,emailTableQueries,emailDataSource, accountQueries, store)
+
+
+
 
         return email
     }
@@ -59,28 +63,29 @@ actual class EmailService {
 
     fun fetchEmailBodies(emailAddress: String, emailTableQueries: EmailTableQueries,emailDataSource: EmailDataSource, accountQueries: AccountTableQueries, store: Store): List<Email> {
         val folder = store.getFolder("INBOX").apply { open(Folder.READ_ONLY) }
-        val messages: List<Message> = folder.messages.slice(0..10)
-
+        println("Number of messages: ${folder.messageCount}")
+        val messages: List<Message> = folder.messages.toList()
         var emails: List<Email> = emptyList()
 
         // Account
         val account = accountQueries.selectAccount(emailAddress = emailAddress).executeAsList()
 
 
-        for (message in messages) {
-            emails += (Email(
-               from = message.from?.joinToString(), subject = message.subject?: "", body = getEmailBody(message), to = "", cc = null, bcc = null, account = account[0]
-            ))
+        for ((index, message) in messages.withIndex()) {
+            print(message.subject)
+//            emails += (Email(
+//               from = message.from?.joinToString(), subject = message.subject?: "", body = getEmailBody(message), to = "", cc = null, bcc = null, account = account[0]
+//            ))
 
-            emailTableQueries.insertEmail(
-                from_user = message.from?.joinToString() ?: "",
-                subject = message.subject ?: "",
-                body = getEmailBody(message),
-                to_user = "",
-                cc = null,
-                bcc = null,
-                account = account[0]
-            )
+//            emailTableQueries.insertEmail(
+//                from_user = message.from?.joinToString() ?: "",
+//                subject = message.subject ?: "",
+//                body = getEmailBody(message),
+//                to_user = "",
+//                cc = null,
+//                bcc = null,
+//                account = account[0]
+//            )
         }
 
         folder.close(false)
