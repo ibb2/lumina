9b255478e273ecef30f2e60b4c8356027aee8433
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-12-20 02:25:05 +1100

    Increased padding for email item.

--- composeApp/src/desktopMain/kotlin/org/example/project/ui/platformSpecific/Card.desktop.kt
+++ composeApp/src/desktopMain/kotlin/org/example/project/ui/platformSpecific/Card.desktop.kt
@@ -2,9 +2,12 @@ package org.example.project.ui.platformSpecific
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.konyaco.fluent.surface.Card
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
@@ -13,7 +16,7 @@ import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 actual fun PlatformSpecificCard(modifier: Modifier, content: @Composable () -> Unit) {
 
     Card(modifier) {
-        Row(horizontalArrangement = Arrangement.spacedBy(16.dp), verticalAlignment = Alignment.CenterVertically) {
+        Row(modifier = Modifier.padding(8.dp), horizontalArrangement = Arrangement.spacedBy(16.dp), verticalAlignment = Alignment.CenterVertically) {
             content()
         }
     }
