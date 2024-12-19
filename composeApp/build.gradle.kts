26d2c7e65df27dc2c4378dc2172de76682a5751e
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-12-20 00:15:07 +1100

    Import fluent ui package.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -72,6 +72,9 @@ kotlin {
             implementation("io.ktor:ktor-server-core:3.0.1")
             implementation("io.ktor:ktor-server-netty:3.0.1")
             implementation("io.ktor:ktor-client-content-negotiation:3.0.1")
+            implementation("com.konyaco:fluent:0.0.1-dev.8")
+            implementation("com.konyaco:fluent-icons-extended:0.0.1-dev.8") // If you want to use full fluent icons.
+
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
